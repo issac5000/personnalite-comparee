230770833a3cc9dfec1406779c0a635512417a3b
issac5000 <ashot.avakian5000@gmail.com>
2025-08-18 04:24:22 +0200

    Avoid overwriting missing translations

--- public/i18n.js
+++ public/i18n.js
@@ -4,7 +4,9 @@ function applyTranslations() {
   document.querySelectorAll('[data-i18n]').forEach(el => {
     const key = el.getAttribute('data-i18n');
     const translation = translations[lang] && translations[lang][key];
-    el.innerHTML = translation !== undefined ? translation : key;
+    if (translation !== undefined) {
+      el.innerHTML = translation;
+    }
   });
 }
 
